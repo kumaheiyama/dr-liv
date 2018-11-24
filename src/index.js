47b7e382d515cc2dfaf3729640c0b5d0e8e018d5
kumaheiyama <bjorn.fredberg@gmail.com>
2018-11-24 20:12:29 +0100

    First working version

--- src/index.js
+++ src/index.js
@@ -1,41 +1,69 @@
 $(function() {
-    var patients = [
-        { imageUrl: 'images/patients/01_teddy_bear.png' }
-    ];
-    var currentPatient = patients[Math.floor(Math.random() * patients.length)];
-    var ailments = [
-        { name: 'fever', imageUrl: 'images/ailments/01_fever.png',
-            remedies: ['sleep', 'rest', 'lots_of_water', 'pain_killer'] },
-        { name: 'tummy ache', imageUrl: 'images/ailments/02_tummy_ache.png',
-            remedies: ['toilet', 'plums', 'lots_of_water'] },
-        { name: 'runny nose', imageUrl: 'images/ailments/03_runny_nose.png',
-            remedies: ['sleep', 'rest', 'wait'] },
-        { name: 'spots', imageUrl: 'images/ailments/04_spots.png',
-            remedies: ['chickenpox_medicine'] }
-    ];
-    var currentAilment = ailments[Math.floor(Math.random() * ailments.length)];
-    $(".current_patient img").attr('src', currentPatient.imageUrl);
-    $(".current_ailment img").attr('src', currentAilment.imageUrl);
- 
-    var remedies = [
-        { name: 'toilet', imageUrl: 'images/remedies/01_toilet.png' },
-        { name: 'hug', imageUrl: 'images/remedies/02_hug.png' },
-        { name: 'chickenpox_medicine', imageUrl: 'images/remedies/03_chickpox_medicine.png' },
-        { name: 'banana', imageUrl: 'images/remedies/04_banana.png' },
-        { name: 'pain_killer', imageUrl: 'images/remedies/05_pain_killer.png' },
-        { name: 'plums', imageUrl: 'images/remedies/06_plums.png' },
-        { name: 'socks', imageUrl: 'images/remedies/07_socks.png' },
-        { name: 'icecream', imageUrl: 'images/remedies/08_icecream.png' },
-        { name: 'rest', imageUrl: 'images/remedies/09_rest.png' },
-        { name: 'wait', imageUrl: 'images/remedies/10_wait.png' },
-        { name: 'cough_medicine', imageUrl: 'images/remedies/11_cough_medicine.png' },
-        { name: 'sleep', imageUrl: 'images/remedies/12_sleep.png' },
-        { name: 'lots_of_water', imageUrl: 'images/remedies/13_lots_of_water.png' },
-        { name: 'oatmeal', imageUrl: 'images/remedies/14_oatmeal.png' }
-    ];
-    _.forEach(remedies, function(remedy) {
-        $("#remedies").append($("<img>", { 'class': 'draggable remedy ' + remedy.name, 'src': remedy.imageUrl }));
-    });
+    var patients = [];
+    var currentPatient = null;
+    var ailments = [];
+    var currentAilment = null;
+    var remedies = [];
+    function init() {
+        patients = [
+            { imageUrl: 'images/patients/01_teddy_bear.png' }
+        ];
+        currentPatient = patients[Math.floor(Math.random() * patients.length)];
+        $(".current_patient img").attr('src', currentPatient.imageUrl);
+
+        ailments = [
+            // { name: 'fever', imageUrl: 'images/ailments/01_fever.png',
+            //     remedies: ['sleep', 'rest', 'lots_of_water', 'pain_killer'] },
+            { name: 'tummy ache', imageUrl: 'images/ailments/02_tummy_ache.png',
+                remedies: ['toilet', 'plums', 'lots_of_water'] },
+            // { name: 'runny nose', imageUrl: 'images/ailments/03_runny_nose.png',
+            //     remedies: ['sleep', 'rest', 'wait'] },
+            { name: 'spots', imageUrl: 'images/ailments/04_spots.png',
+                remedies: ['chickenpox_medicine'] }
+        ];
+        currentAilment = ailments[Math.floor(Math.random() * ailments.length)];
+        $(".current_ailment img").attr('src', currentAilment.imageUrl);
+    
+        remedies = [
+            { name: 'toilet', imageUrl: 'images/remedies/01_toilet.png' },
+            { name: 'hug', imageUrl: 'images/remedies/02_hug.png' },
+            { name: 'chickenpox_medicine', imageUrl: 'images/remedies/03_chickenpox_medicine.png' },
+            { name: 'banana', imageUrl: 'images/remedies/04_banana.png' },
+            { name: 'pain_killer', imageUrl: 'images/remedies/05_pain_killer.png' },
+            { name: 'plums', imageUrl: 'images/remedies/06_plums.png' },
+            { name: 'socks', imageUrl: 'images/remedies/07_socks.png' },
+            { name: 'icecream', imageUrl: 'images/remedies/08_icecream.png' },
+            { name: 'rest', imageUrl: 'images/remedies/09_rest.png' },
+            { name: 'wait', imageUrl: 'images/remedies/10_wait.png' },
+            { name: 'cough_medicine', imageUrl: 'images/remedies/11_cough_medicine.png' },
+            { name: 'sleep', imageUrl: 'images/remedies/12_sleep.png' },
+            { name: 'lots_of_water', imageUrl: 'images/remedies/13_lots_of_water.png' },
+            { name: 'oatmeal', imageUrl: 'images/remedies/14_oatmeal.png' }
+        ];
+        $("#remedies").empty();
+        _.forEach(remedies, function(remedy) {
+            var remedyClass = 'draggable remedy ' + remedy.name;
+            if (_.includes(currentAilment.remedies, remedy.name)) {
+                remedyClass += ' acceptedRemedy';
+            }
+            $("#remedies").append($("<img>", { 'class': remedyClass, 'src': remedy.imageUrl }));
+        });
+
+        var acceptedRemedies = '.acceptedRemedy';
+        var canGoToNextPatient = false;
+        $(".droppableRemedy img").droppable({
+            accept: acceptedRemedies,
+            classes: {
+                'ui-droppable-active': 'ui-state-default'
+            },
+            drop: function(event, ui) {
+                //$(this).addClass('ui-state-highlight');
+                $(".next_patient").show();
+            }
+        });
+        $(".next_patient").hide();
+    }
+    init();
 
     $(".draggable").draggable(
         {
@@ -46,15 +74,7 @@ $(function() {
             opacity: 0.7
         }
     );
-    $(".droppableRemedy").droppable({
-        accept: '.remedy',
-        classes: {
-            'ui-droppable-active': 'ui-state-default'
-        },
-        drop: function(event, ui) {
-            $(this).addClass('ui-state-highlight')
-                .find('p')
-                .html('dropped');
-        }
+    $(".next_patient").click(function() {
+        init();
     });
 } );
