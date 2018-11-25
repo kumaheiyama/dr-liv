311e222caebdfcc6a8b48a94247fd27bc2ba3695
kumaheiyama <bjorn.fredberg@gmail.com>
2018-11-25 21:44:17 +0100

    New graphics

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ All art is by Malin, with coloring by Liv and her sister Alva.
 1. The patient arrives (looking ill).
 2. The doctor (you) gathers the symptoms.
 3. After diagnosis the doctor administers different remedies depending on the ailment.
-4. If correct remedy, the patient gets better and the doctor can see the next patient.
+4. If correct remedy, the patient gets better.
+5. The doctor can see the next patient.
+
+Since the game is aimed at children below a reading age it features no written language but instead uses emoji for conveying feelings, instructions and feedback.
--- src/index.js
+++ src/index.js
@@ -1,44 +1,50 @@
 $(function() {
+    var basePatiensFolder = 'images/patients/';
+    var baseRemediesFolder = 'images/remedies/';
+    var baseAilmentFolder = 'images/ailments/';
     var patients = [];
     var currentPatient = null;
     var ailments = [];
     var currentAilment = null;
     var remedies = [];
+
     function init() {
         patients = [
-            { imageUrl: 'images/patients/01_teddy_bear.png' }
+            { imageUrl: '01_teddy_bear.png' }
         ];
         currentPatient = patients[Math.floor(Math.random() * patients.length)];
-        $(".current_patient img").attr('src', currentPatient.imageUrl);
+        $(".current_patient img").attr('src', basePatiensFolder + currentPatient.imageUrl);
 
         ailments = [
-            { name: 'fever', imageUrl: 'images/ailments/01_fever.png',
+            { name: 'fever', imageUrl: '01_fever.png',
                 remedies: ['sleep', 'rest', 'lots_of_water', 'pain_killer'] },
-            { name: 'tummy ache', imageUrl: 'images/ailments/02_tummy_ache.png',
+            { name: 'tummy ache', imageUrl: '02_tummy_ache.png',
                 remedies: ['toilet', 'plums', 'lots_of_water'] },
-            { name: 'runny nose', imageUrl: 'images/ailments/03_runny_nose.png',
-                remedies: ['sleep', 'rest', 'wait'] },
-            { name: 'spots', imageUrl: 'images/ailments/04_spots.png',
+            { name: 'runny nose', imageUrl: '03_runny_nose.png',
+                remedies: ['sleep', 'rest', 'wait', 'tissues'] },
+            { name: 'spots', imageUrl: '04_spots.png',
                 remedies: ['chickenpox_medicine'] }
         ];
         currentAilment = ailments[Math.floor(Math.random() * ailments.length)];
-        $(".current_ailment img").attr('src', currentAilment.imageUrl);
+        $(".current_ailment img").attr('src', baseAilmentFolder + currentAilment.imageUrl);
     
         remedies = [
-            { name: 'toilet', imageUrl: 'images/remedies/01_toilet.png' },
-            { name: 'hug', imageUrl: 'images/remedies/02_hug.png' },
-            { name: 'chickenpox_medicine', imageUrl: 'images/remedies/03_chickenpox_medicine.png' },
-            { name: 'banana', imageUrl: 'images/remedies/04_banana.png' },
-            { name: 'pain_killer', imageUrl: 'images/remedies/05_pain_killer.png' },
-            { name: 'plums', imageUrl: 'images/remedies/06_plums.png' },
-            { name: 'socks', imageUrl: 'images/remedies/07_socks.png' },
-            { name: 'icecream', imageUrl: 'images/remedies/08_icecream.png' },
-            { name: 'rest', imageUrl: 'images/remedies/09_rest.png' },
-            { name: 'wait', imageUrl: 'images/remedies/10_wait.png' },
-            { name: 'cough_medicine', imageUrl: 'images/remedies/11_cough_medicine.png' },
-            { name: 'sleep', imageUrl: 'images/remedies/12_sleep.png' },
-            { name: 'lots_of_water', imageUrl: 'images/remedies/13_lots_of_water.png' },
-            { name: 'oatmeal', imageUrl: 'images/remedies/14_oatmeal.png' }
+            { name: 'toilet', imageUrl: '01_toilet.png' },
+            { name: 'hug', imageUrl: '02_hug.png' },
+            { name: 'chickenpox_medicine', imageUrl: '03_chickenpox_medicine.png' },
+            { name: 'banana', imageUrl: '04_banana.png' },
+            { name: 'pain_killer', imageUrl: '05_pain_killer.png' },
+            { name: 'plums', imageUrl: '06_plums.png' },
+            { name: 'socks', imageUrl: '07_socks.png' },
+            { name: 'icecream', imageUrl: '08_icecream.png' },
+            { name: 'rest', imageUrl: '09_rest.png' },
+            { name: 'wait', imageUrl: '10_wait.png' },
+            { name: 'cough_medicine', imageUrl: '11_cough_medicine.png' },
+            { name: 'sleep', imageUrl: '12_sleep.png' },
+            { name: 'lots_of_water', imageUrl: '13_lots_of_water.png' },
+            { name: 'oatmeal', imageUrl: '14_oatmeal.png' },
+            { name: 'food', imageUrl: '15_food.png'},
+            { name: 'tissues', imageUrl: '16_tissues.png'}
         ];
         $("#remedies").empty();
         _.forEach(remedies, function(remedy) {
@@ -46,7 +52,7 @@ $(function() {
             if (_.includes(currentAilment.remedies, remedy.name)) {
                 remedyClass += ' acceptedRemedy';
             }
-            $("#remedies").append($("<img>", { 'class': remedyClass, 'src': remedy.imageUrl }));
+            $("#remedies").append($("<img>", { 'class': remedyClass, 'src': baseRemediesFolder + remedy.imageUrl }));
         });
 
         $(".draggable").draggable(
@@ -59,14 +65,13 @@ $(function() {
             }
         );
         var acceptedRemedies = '.acceptedRemedy';
-        var canGoToNextPatient = false;
         $(".droppableRemedy img").droppable({
             accept: acceptedRemedies,
             classes: {
                 'ui-droppable-active': 'ui-state-default'
             },
             drop: function(event, ui) {
-                //$(this).addClass('ui-state-highlight');
+                //TODO $(this).addClass('ui-state-highlight');
                 $(".next_patient").show();
             }
         });
